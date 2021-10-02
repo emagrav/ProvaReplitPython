e1c64d9eb5be301dbbfb336c2f4c3a05e0382ae1
emagrav <emagrav@gmail.com>
2021-10-02 19:20:09 +0200

    ulteriori esempi

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 print('Hello, world!')
-a="pippo"
-print(a.capitalize());
 for x in range(10):
   print(x)
 print ("anvedi!!")
- 
+###
+nome="Ele"
+citta="RoMa"
+frase = "Mi chIAMo {}, sono nato a {} e vivO a {}".format(nome,citta,citta)
+print(frase.upper())
+print(frase.capitalize())
+print(frase.lower())
+print(frase)
+print(frase.find("ELE"))
+print(frase.find("Ele"))
+print("ELE" in frase)
+print("ELE".capitalize() in frase)
+frase2 = frase.replace(frase[-4:],"TarANTo",1)
+print(frase2)
+frase2 = frase.replace(frase[-4:],"TarANTo")
+print(frase2)
