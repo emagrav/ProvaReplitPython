12fa7730a003e515c0e9fda71ffbb485db04286a
emagrav <emagrav@gmail.com>
2021-10-03 22:05:21 +0200

    aggiunti commenti

--- primi_passi.py
+++ primi_passi.py
@@ -3,17 +3,29 @@ for x in range(10):
   print(x)
 print ("anvedi!!")
 ###
+
+############################
+# alcune funzioni delle stringhe
+############################
 nome="Ele"
 citta="RoMa"
 frase = "Mi chIAMo {}, sono nato a {} e vivO a {}".format(nome,citta,citta)
 print(frase.upper())
-print(frase.capitalize())
+print(frase.capitalize()) # capitalize rende la prima maiuscola e il resto minuscolo.
 print(frase.lower())
 print(frase)
-print(frase.find("ELE"))
+
+# il find è case sensitive e mi restituisce la pos. della sottostringa o -1
+print(frase.find("ELE")) 
 print(frase.find("Ele"))
+
+# se mi interessa sapere solo se è contenuta una stringa ma non mi interessa la sua posizione posso
+# usare + semplicemente l'istruzione "insiemistica" 'in'
 print("ELE" in frase)
 print("ELE".capitalize() in frase)
+
+# il 3° parametro della replace dice quante occorrenze della sottostringa contenuta nella stringa deve 
+# essere sostituita. Se non è presente la repace agisce su tutte.
 frase2 = frase.replace(frase[-4:],"TarANTo",1)
 print(frase2)
 frase2 = frase.replace(frase[-4:],"TarANTo")
--- standard_library.py
+++ standard_library.py
@@ -1,7 +1,11 @@
-#import random 
-from random import randint
+#import random               # importo modulo random. Per poter utilizzare tutte le sue funzioni
+                            # devo far precedere il nome di ciascuna funzione da random
+from random import randint  # qui invece importo solo funzione radint del modulo random e non
+                            # devo far precedere il nome della funzione da random
+#from random import *        # qui invece importo tutte le funzioni del modulo random e non
+                            # devo far precedere il nome di ciascuna funzione da random
 
 for n in range(10):
-    #val = random.randint(1,50)
-    val = randint(1,50)
+    #val = random.randint(1,50) # caso import random
+    val = randint(1,50)         # caso from random import radint (ovvero from random import *)
     print(val)
