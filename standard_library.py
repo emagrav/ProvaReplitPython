379abfc19b202fd905ce79222968c1784fc4f96b
emagrav <emagrav@gmail.com>
2021-10-03 19:30:59 +0200

    ok

--- standard_library.py
+++ standard_library.py
@@ -1,5 +1,7 @@
-import random 
+#import random 
+from random import randint
 
 for n in range(10):
-    val = random.randint(1,50)
+    #val = random.randint(1,50)
+    val = randint(1,50)
     print(val)
