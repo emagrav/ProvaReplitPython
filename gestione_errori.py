6cc82c275dab9857c99308d37d40abbfae43bc5e
emagrav <emagrav@gmail.com>
2021-10-04 19:33:27 +0200

    commento

--- gestione_errori.py
+++ gestione_errori.py
@@ -10,7 +10,7 @@ divisore(5,0)
 
 def moltiplicatore():
     try:
-        a=int(input('inserisci il primo fattore: '))
+        a=int(input('inserisci il primo fattore: ')) #ValueError lo becchi anche passando 2.5 in quanto int(2.5) restiruisce 2 mentre int("2.5") da errore. Andrebbe risolto così in caso di necessità int(float("2.5"))
         b=int(input('inserisci il secondo fattore: '))
         ris = a * b
         print("Risultato:", ris)
