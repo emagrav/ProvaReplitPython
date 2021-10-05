e8c412eadc91b46fbfa8297ea919ef6e8aeda02b
E G <emagrav@gmail.com>
2021-10-05 12:11:11 +0000

    ultimo esempio su file

--- file_zip_esempi.py
+++ file_zip_esempi.py
@@ -1,6 +1,6 @@
 print("************FILE ZIP ESEMPI ****************")
 
-#import os
+import os
 import zipfile
 
 '''
@@ -19,7 +19,8 @@ archivio.write("Files/trapano.wav", compress_type=zipfile.ZIP_DEFLATED) #ci inse
 archivio.close() #chiudo lo zip
 '''
 
-archivio = zipfile.ZipFile("Files/archivio_prova.zip")
+archivio = zipfile.ZipFile("Files/archivio_prova.zip") #apro in lettura
+#archivio = zipfile.ZipFile("Files/archivio_prova.zip","r")
 
 print(archivio.namelist()) #namelist fornisce la lista dei file contenuti nello zip
 
@@ -27,4 +28,9 @@ print(archivio.namelist()) #namelist fornisce la lista dei file contenuti nello 
 archivio.extractall("Files/ZipEstratto") #estraggo il file zip
 '''
 
+#verifica come fare per estrarre un singolo file dallo zip
+
 archivio.close()
+
+fsize=os.stat("Files/archivio_prova.zip")
+print("Dimensione:",fsize.st_size.__str__()) # interrogo le statistiche su un file, in particolare la dimensione di un file
