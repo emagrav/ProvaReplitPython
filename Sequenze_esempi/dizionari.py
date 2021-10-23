0568f0fa51c4d8586c1fb9eec2f4f3350db9c06d
emagrav <emagrav@gmail.com>
2021-10-23 23:35:37 +0200

    Refactoring ulteriore

--- Sequenze_esempi/dizionari.py
+++ Sequenze_esempi/dizionari.py
@@ -1,12 +1,14 @@
 print("*************DIZIONARI**********")
 
-mio_diz = {"key1":"val1","eta":24, 3.14:"pi_greco", "primi":[1,3,5,7]}
+# da questo esempio si capisce che le chiavi possono essere anche numeri
+# non solo stringhe
+mio_diz = {"key1":"val1", "eta":24, 3.14:"pi_greco", "primi":[1, 3, 5, 7]}
 
 print(mio_diz["key1"])
 print(mio_diz[3.14])
 
 #aggiungo nuovo elemento
-mio_diz["new_key"]="new_val"
+mio_diz["new_key"] = "new_val"
 print(mio_diz)
 
 print("primi" in mio_diz) #verifico se una chiave è censita
@@ -31,20 +33,20 @@ for k in mio_diz.keys():
     print(k)
 
 #get per gestire l'assenza di una chiave cercata per ottenere il corrispondente valore
-print(mio_diz.get(3.14,"Voce non trovata nel dizionario"))
-print(mio_diz.get("birra","Voce non trovata nel dizionario"))
+print(mio_diz.get(3.14, "Voce non trovata nel dizionario"))
+print(mio_diz.get("birra", "Voce non trovata nel dizionario"))
 
 #setdefault serve come get per gestire l'assenza ma anziché restituire un messaggio di default, assegna un nuovo elemento con la coppia chiave-valore indicata.
 #qualora la chiave esistesse già, restituirebbe il valore già associato e non quello passato in questa istruzione
-mio_diz.setdefault(3.14,"pi greco!!!")
-mio_diz.setdefault("birra","Corona Extra")
+mio_diz.setdefault(3.14, "pi greco!!!")
+mio_diz.setdefault("birra", "Corona Extra")
 
 print(mio_diz)
 
 ########
 diction = {}
 diction["a"] = "abello"
-diction["b"] = "brutto"
+diction["b"] = "abrutto"
 diction["k"] = "ketteridi"
 
 b=diction.copy() #creo una copia del dizionario
